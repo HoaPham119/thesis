0c10883b6908174959e8a7289588cc1cf55d4462
Hoà Phạm <hoapham@Hoas-MacBook-Pro.local>
2025-04-07 19:26:52 +0700

    add get data from yfinance function

--- code/data_load.py
+++ code/data_load.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import yfinance as yf
 from pathlib import Path
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
@@ -21,6 +22,23 @@ def load_data(folder="tick"):
             data_dict[name] = pickle.load(f)
     return data_dict
 
+def get_data_from_yfinance():
+
+    # Lấy dữ liệu của S&P 500
+    sp500 = yf.Ticker("^GSPC")
+
+    # Xem thông tin chung
+    print(sp500.info)
+
+    # Lấy dữ liệu lịch sử (ví dụ: 1 năm)
+    data = sp500.history(period="1y")
+    print(data.head())
+
+    # Lấy giá đóng cửa gần nhất
+    latest_close = data["Close"].iloc[-1]
+    print("Giá đóng cửa gần nhất:", latest_close)
+
 
 if __name__ == "__main__":
+    get_data_from_yfinance()
     load_data(folder="")
