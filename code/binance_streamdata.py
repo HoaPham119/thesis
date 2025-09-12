833ccbcd54a973fc1f3fc5be1750f8fb1d757731
Pham <hoa.pham_ic@wwwinc.com>
2025-09-12 15:29:21 +0700

    binance stream data

--- code/binance_streamdata.py
+++ code/binance_streamdata.py
@@ -4,18 +4,20 @@ import websockets
 import pandas as pd
 
 symbols = ["BNBUSDT"] 
+raw_cols = ["a", "p", "q", "f", "l", "T", "m", "M"]
 stream_names = "/".join(f"{s.lower()}@aggTrade" for s in symbols)
 
 WS_COMBINED = f"wss://stream.binance.com:9443/stream?streams={stream_names}"
 
 async def handle_combined(msg, df):
     data = msg.get("data", {})
-    data = {k: data[k] for k in ["a", "p", "q", "f", "l", "T", "m", "M"]}
+    data = {k: data[k] for k in raw_cols}
+    print(data)
     df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
     return df
 
 async def run():
-    df = pd.DataFrame(columns=["a","p","q","f","l","T","m","M"])
+    df = pd.DataFrame(columns=raw_cols)
     async with websockets.connect(WS_COMBINED, ping_interval=20) as ws:
         async for raw in ws:
             msg = json.loads(raw)
